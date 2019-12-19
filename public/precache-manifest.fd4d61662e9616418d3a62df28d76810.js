74e54ef23b84e930b6a8f5fcee7ea2e720f053cb
jakubhimmel <jakubhimmel94@gmail.com>
2019-12-19 15:17:57 +0100

    final3

---
+++ public/precache-manifest.fd4d61662e9616418d3a62df28d76810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6268e8c0a56e8e202489dba524c88acc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1612a0409673a2d4f3da",
+    "url": "/static/css/main.9dfc7429.chunk.css"
+  },
+  {
+    "revision": "1d18b306fb87547c623e",
+    "url": "/static/js/2.4be4c6b5.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.4be4c6b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1612a0409673a2d4f3da",
+    "url": "/static/js/main.0336d3cc.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  }
+]);
